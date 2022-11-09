f9d3fe525bec79a57d8acae51b7726299f923dc5
Mehedi hassan <mh2863268@gmail.com>
2022-11-09 19:40:40 +0600

    completed

--- src/components/adduser.js
+++ src/components/adduser.js
@@ -1,5 +1,4 @@
 import React,{ useState, useEffect } from 'react';
-import propTypes from 'prop-types'
 
 const Adduser = ({handleSubmitAdd, remove, btnText}) => {
     const [user, setUser] = useState(
