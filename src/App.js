bbd9a80f1c07a74a03df73e86662f9b3aa011a5c
Mehedi hassan <mh2863268@gmail.com>
2022-11-09 18:53:59 +0600

    at last finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React,{ useState, useEffect} from 'react';
 import './App.css';
+import Adduser from './components/adduser';
 
 const url="https://rest-api-without-db.herokuapp.com/users/";
 function App() {
   const [users, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState();
+  const [error, setError] = useState(null);
+
+  //For removing data
+  const [remove, setRemove] = useState({
+    username: "",
+    email: "",
+  });
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [selectedUpdaterId, setSelectedUpdaterId] = useState(false);
+
 
 const allUser = () =>{
   fetch(url)
   .then((res)=> {
-  if(!res){
+  if(!res.ok){
     throw Error ("The error is not found")
   }
   return res.json()
@@ -36,21 +46,71 @@ const allUser = () =>{
       method: "DELETE",
     })
     .then((res)=> {
-    if(!res){
+    if(!res.ok){
       throw Error ("COULD NOT DELETE")
     }
     allUser();
     })
+  }
+
+  const addUser =(user)=>{
+    fetch(url,{
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(user),
+    })
+    .then((res)=> {
+    if(!res.ok){
+      throw Error("COULD NOT CREATE")
+    }else
+    {allUser();}
+    })
+    .catch((err)=>{
+      setError(err.message)
+    })
+  }
+  const selectedUser= (id)=>{
+    setSelectedUpdaterId(id);
+    const editFiltered=users.filter((user)=>user.id === id)
+    setRemove({
+      username: editFiltered[0].username,
+      email: editFiltered[0].email,
+  })
+  setIsUpdate(true);
+  }
+
+  const handleUpdate=(user)=>{
+    fetch(url+`/${selectedUpdaterId}`,{
+      method: "PUT",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(user),
+    })
+    .then((res)=> {
+    if(!res.ok){
+      throw Error("COULD NOT UPDATE")
+    }else
+    {allUser();}
+    setIsUpdate(false);
+    })
+    .catch((err)=>{
+      setError(err.message)
+    })
+
   }
    
   return (
     <div className="App">
       <h1>The data management sytem</h1>
       {isLoading && <h2>Data is loading...</h2>}
-      {error && (error)}
-      <form>
-        <input></input>
-      </form>
+      {error && <h1>{error}</h1>}
+      {isUpdate ? <Adduser 
+      handleSubmitAdd={handleUpdate}
+      btnText="Update User" 
+      remove={remove}/> : <Adduser handleSubmitAdd={addUser} btnText="Add user"  />}
       <section className="section">
       {users && users.map((user)=> {
         const {id, username, email}= user;
@@ -58,7 +118,7 @@ const allUser = () =>{
           <article key={id} className="card">
             <h3>{username}</h3>
             <p>{email}</p>
-            <button className="btn">Add</button>
+            <button className="btn" onClick={()=>{selectedUser(id)}}>Edit</button>
             <button className="btn" onClick={()=>{handleDelete(id)}}>Delete</button>
           </article>
         )
---
+++ src/components/adduser.js
@@ -0,0 +1,54 @@
+import React,{ useState, useEffect } from 'react';
+import propTypes from 'prop-types'
+
+const Adduser = ({handleSubmitAdd, remove, btnText}) => {
+    const [user, setUser] = useState(
+    {    username: "",
+         email: "", }
+    )
+    const{ username, email} = user;
+    useEffect(() => {
+        setUser({
+            username: remove.username,
+            email: remove.email,
+        })
+    }, [remove])
+    
+
+    const handleChange=(e)=> {
+        const selectedfield = e.target.name;
+        const selectedvalue = e.target.value;
+        setUser(prevstate=>{
+            return {...prevstate,[selectedfield]: selectedvalue}
+        });
+    }
+    const handleSubmit= (e)=>{
+        e.preventDefault();
+        handleSubmitAdd(user);
+        setUser({
+            username: "",
+            email: "",
+        })
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="field-group">
+                <label  htmlFor="username" >UserName:</label>
+                <input className="userInput" id="username" placeholder='username' type="text" name="username" value={username} onChange={handleChange} required></input>
+            </div>
+            <div className="field-group">
+                <label  htmlFor="email" >Email:</label>
+                <input className="userInput" id="email" type="text" placeholder='email' name="email" value={email} onChange={handleChange} required></input>
+            </div>
+            <button type="submit" className="btn">{btnText}</button>
+        </form>
+    );
+};
+Adduser.defaultProps={
+    remove: {
+        username: "",
+        email: "",
+    }
+}
+
+export default Adduser;
